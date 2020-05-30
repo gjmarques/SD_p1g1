19339cb6162492b0eef15b0fa3e3f57152a81d52
Joana Bernardino <joanagomesbernardino@ua.pt>
2020-05-30 17:56:44 +0100

    separated compiling and running

--- project2/SD_P1G1_p2/compile.sh
+++ project2/SD_P1G1_p2/compile.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-javac mainProgram/AirportMain.java
+echo "Compiling..."
+#compile entities mains
+javac clientSide/PassengerMain.java
+javac clientSide/BusDriverMain.java
+javac clientSide/PorterMain.java
+# compile shared regions mains
+javac serverSide/ALMain.java
+javac serverSide/ATEMain.java
+javac serverSide/ATTQMain.java
+javac serverSide/BCPMain.java
+javac serverSide/BROMain.java
+javac serverSide/DTEMain.java
+javac serverSide/DTTQMain.java
+javac serverSide/GIRMain.java
+javac serverSide/TSAMain.java
--- project2/SD_P1G1_p2/run.sh
+++ project2/SD_P1G1_p2/run.sh
@@ -1,21 +1,5 @@
 #!/bin/bash
 
-echo "Compiling..."
-#compile entities mains
-javac clientSide/PassengerMain.java
-javac clientSide/BusDriverMain.java
-javac clientSide/PorterMain.java
-# compile shared regions mains
-javac serverSide/ALMain.java
-javac serverSide/ATEMain.java
-javac serverSide/ATTQMain.java
-javac serverSide/BCPMain.java
-javac serverSide/BROMain.java
-javac serverSide/DTEMain.java
-javac serverSide/DTTQMain.java
-javac serverSide/GIRMain.java
-javac serverSide/TSAMain.java
-
 echo "Running..."
 # run shared regions mains
 gnome-terminal -e "java serverSide/GIRMain" -t "GIRMain"
